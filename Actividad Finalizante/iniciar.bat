10c9ff80ccee049de2a427c169e802ee5e5cff6c
istic4lex <atinco@iiconcepcion.edu.ar>
2025-11-20 00:05:48 -0300

    Delete Actividad Finalizante directory

--- Actividad Finalizante/iniciar.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start C:\php\php.exe -S 0.0.0.0:3000 -t publica/
-start chrome http://localhost:3000
--- Actividad Finalizante/publica/clientes/todos/render.php
+++
@@ -1,9 +0,0 @@
-<article id="idArticulo_<?= $cadaRegistro->id ?>" >
-    <h2><?= $cadaRegistro->nombreCliente ?></h2>
-    <nav>
-        <button onclick="location.href='/clientes/ver?id=<?= $cadaRegistro->id ?>'">Más información</button>
-        <button onclick="location.href='/clientes/editar?id=<?= $cadaRegistro->id ?>'">Editar</button>
-        <button onclick="location.href='/clientes/borrar?id=<?= $cadaRegistro->id ?>'">Borrar</button>
-    </nav>
-</article>
-
